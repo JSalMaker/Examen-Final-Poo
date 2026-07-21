20abeaa1de7d0dfa2aea6c1be2427a5ae31fe71a
Laura Espinosa <laespinosa@unal.edu.co>
2026-07-20 21:51:45 -0500

    Add files via upload
    ParkingLot

---
+++ parking/parking_lot.py
@@ -0,0 +1,60 @@
+from queue import Queue
+from datetime import datetime
+class ParkingLot:
+    def __init__(self, name: str, slots: list):
+        self.name = name
+        self._slots= slots
+        self._waiting_queue = Queue()
+
+    def park_vehicle(self, vehicle):
+        for slot in self._slots:
+            if not slot.is_occupied():
+                slot.park(vehicle)
+                
+                entry_time = datetime.now()
+                ticket = Ticket(vehicle, slot.slot_id, entry_time)
+                
+                return ticket
+               
+        self._waiting_queue.put(vehicle)
+        raise ParkingFullError("No hay cupos disponibles, se encuentra en lista de espera.")
+
+
+    def exit_vehicle(self, plate):
+        for slot in self._slots:
+            if slot.is_occupied():
+                vehicle = slot.get_vehicle()
+
+                if vehicle.license_plate == plate:
+                    slot.vacate()
+
+                    if not self._waiting_queue.empty():
+                        next_v = self._waiting_queue.get()
+                        slot.park(next_v)
+
+                    return
+                
+        raise VehicleNotFoundError(f"El vehiculo con placa {plate} no fue encontrado.")
+                    
+    def available_count(self):
+        counter = 0
+        available_slots = []
+
+        for slot in self._slots:
+            if not slot.is_occupied():
+                available_slots.append(slot.slot_id)
+                counter +=1
+
+        print(f"Parqueaderos disponibles: {counter}")
+        print("Los ID de éstos son:")
+
+        for i, available_slot in enumerate(available_slots, start = 1):
+            print(i, available_slot)
+
+        return counter
+
+    def occupied_vehicles(self):
+        for slot in self._slots:
+            if slot.is_occupied():
+                yield slot.slot_id, slot.get_vehicle()
+    
