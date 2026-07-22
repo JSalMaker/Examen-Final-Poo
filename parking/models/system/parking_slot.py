fb75c20fbde9f2ce0cf0c42a02e35ed17ef97fb9
Laura Espinosa <laespinosa@unal.edu.co>
2026-07-21 20:08:46 -0500

    Contribuciones grupales

--- parking/models/system/parking_slot.py
+++ parking/models/system/parking_slot.py
@@ -1,17 +1,19 @@
-from exceptions.slot_occupied_error import SlotOccupiedError
 import uuid
 from models.ticket import Ticket
+from exceptions.slot_occupied_error import SlotOccupiedError
+
 class ParkingSlot:
     def __init__(self, id: str):
-        self._id = str(uuid.uuid1())[:5] 
-        self._vehicle = None 
+        self._id = id
+        self._protected_ref = str(uuid.uuid1())[:5] 
+        self._vehicle = None
         
          
     def __str__(self):
         return ( 
         f"Parqueadero numero :{self._id}\n"
-        f"Parqueadero Ocupado: {self._is_occupied()}\n"
-        f"Vehiculo Obtenido: {self.get_vehicle()}\n"
+        f"Parqueadero Ocupado: {self._is_occupied()}\n" # type: ignore
+        f"{self.get_vehicle()}"
         )
 
     def park(self, vehicle) -> bool:
@@ -19,16 +21,14 @@ class ParkingSlot:
             raise SlotOccupiedError("Puesto ocupado, intente con otro lugar", vehicle)
     
         else:
-            
-            
-                self.set_vehicle(vehicle)
-                vehicle.ticket = Ticket(vehicle, self._id)
-                print(f"El vehiculo de placas {vehicle.license_plate} está parqueando en el espacio {self._id}")
-                print(f"{vehicle.get_name()}, Gracias por ocupar nuestro espacio")
-                return True
+            self.set_vehicle(vehicle)
+            vehicle.ticket = Ticket(vehicle, self._id)
+            print(f"El vehiculo de placas {self._vehicle.license_plate} está parqueando en el espacio {self._id}\n") # type: ignore
+            print(f"{self._vehicle.get_name()}, Gracias por ocupar nuestro espacio\n") # type: ignore
+            return True
             
     def vacate(self):
-        print(f"{self._vehicle.get_name},  Muchas Gracias por su visita\n")
+        print(f"{self._vehicle.get_name()} , muchas gracias por su visita\n") # type: ignore
         self._vehicle = None
 
 
@@ -36,18 +36,10 @@ class ParkingSlot:
         self._vehicle = vehicle
     
     def get_vehicle(self):
-        print(self._vehicle)
+        return(self._vehicle)
     
     def _is_occupied(self):
         if self._vehicle:
             return True
         else: 
             return False
-
-
-
-
-
-
-
-
