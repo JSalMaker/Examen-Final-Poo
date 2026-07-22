3f05ac5faf29c036e9e26fbceae7834ab604af9e
Juan David Salgado Prieto <jsalgadop@unal.edu.co>
2026-07-22 09:35:24 -0500

    Update slot_occupied_error.py

--- parking/exceptions/slot_occupied_error.py
+++ parking/exceptions/slot_occupied_error.py
@@ -2,5 +2,5 @@ from models.vehicle import Vehicle
 class SlotOccupiedError(Exception):
     def __init__(self,message, vehicle: Vehicle):
         super().__init__(message)
-        print(f"vehiculo de placas {vehicle.get_name} estacionado en el slot")
+        print(f"vehiculo de placas {vehicle.get_name()} estacionado en el slot")
     
