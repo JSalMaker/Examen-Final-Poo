61dfc58421e61b82bf7c1df82cfcd5f9e52ce4ad
Laura Espinosa <laespinosa@unal.edu.co>
2026-07-21 20:04:20 -0500

    Contribuciones Grupales Juan David Salgado-Laura Juliana

--- parking/models/ticket.py
+++ parking/models/ticket.py
@@ -1,21 +1,33 @@
-from models.vehicle import Vehicle
+from __future__ import annotations
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.car import Car
+    from models.motorcycle import Motorcycle
 
 class Ticket:
-    def __init__(self, vehicle: Vehicle, slot_id: str):
+    def __init__(self, vehicle: Car | Motorcycle , slot_id: str):
         self.vehicle = vehicle
         self.slot_id = slot_id
+        self.entry_time = datetime.now()
 
-    def calculate_fee(self, hours: float) -> str:
-        if self.vehicle.get_type()== 'Carro':
+    def calculate_fee(self) -> str:
+        from models.car import Car
+
+        time = datetime.now() - self.entry_time
+        seconds = time.total_seconds()
+        hours = seconds/3600
+
+        valor_ticket: float = 0
+        if isinstance(self.vehicle, Car):
             valor_ticket = 5000 * hours
             if self.vehicle._is_member:
-                 valor_ticket = valor_ticket * 0.10
-                 return f"valor ticket: {str(valor_ticket)}"
-    
+                valor_ticket = valor_ticket * 0.90
+        
         else:
             valor_ticket = 3000 * hours
             if self.vehicle._is_member:
-                valor_ticket = valor_ticket * 0.1        
-            return f"valor ticket: {str(valor_ticket)}"
-        return f"valor ticket: {str(valor_ticket)}"
-    
+                valor_ticket = valor_ticket * 0.90
+        
+        return (f"Cobro total: {valor_ticket}")
